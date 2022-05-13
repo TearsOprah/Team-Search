111e4abc06d00e5594c1eae25a85220463de3c66
TearsOprah <qmeshokp@mail.ru>
2022-05-13 17:11:03 +0300

    1. Add auto layout for welcome screen

--- Team Search/ViewController.swift
+++ Team Search/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var logo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
