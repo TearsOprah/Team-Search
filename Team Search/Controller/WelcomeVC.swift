25a3702a33969174e2710b1a5130990112a8b05b
TearsOprah <qmeshokp@mail.ru>
2022-05-18 10:27:18 +0300

    Add VC for league and skill screen

--- Team Search/Controller/WelcomeVC.swift
+++ Team Search/Controller/WelcomeVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     
     @IBOutlet var backgroundImage: UIImageView!
     @IBOutlet var logo: UIImageView!
