1a23cf3620d39e9a3c00ec9eb2dc16365b55f184
TearsOprah <qmeshokp@mail.ru>
2022-05-18 12:07:06 +0300

    Add model Player Add functional desired league buttons

---
+++ Team Search/Model/player.swift
@@ -0,0 +1,13 @@
+//
+//  player.swift
+//  Team Search
+//
+//  Created by meshok on 18.05.2022.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var desiredSkillLevel: String?
+}
